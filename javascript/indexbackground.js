096be900c1f2a453fb1c81b6e2bb544fe394eb60
Lad-Rahul <rahullad945@gmail.com>
2019-01-13 15:35:00 +0530

    new javascript file made for index

---
+++ javascript/indexbackground.js
@@ -0,0 +1,18 @@
+	function init(){
+	// 	$(document).ready(function() {
+ 	//    $(".logodiv").delay(2000).fadeIn(500);
+	// });
+
+	$(".dot").click(function(){
+
+   	$(this).css("box-shadow","0 0 0 30px rgba(255,92,51,.1),0 0 0 60px rgba(255,92,51,.1),0 0 0 120px rgba(255,92,51,.1),0 0 0 240px rgba(255,92,51,.1),0 0 0 480px rgba(255,92,51,.1),0 0 0 960px rgba(255,92,51,.1)");
+
+   	})
+
+	setTimeout(function(){$(".dot").trigger("click")},250);
+	
+
+	$('.dot').delay(500).fadeOut(3000);
+	// $('.logodiv').fadeIn(1000);
+
+}
