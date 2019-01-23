f47900a8a4037d9bba76c9ad178a508bef3be702
Kalp Panwala <kpanwala33@gmail.com>
2019-01-23 23:18:57 +0530

    changed event home page

--- javascript/eventhomejs.js
+++ javascript/eventhomejs.js
@@ -1,3 +1,4 @@
+
 function init_eventhome(){
 
 	sectionTechnical = document.getElementById('section_technical');
@@ -10,25 +11,26 @@ function init_eventhome(){
 	btnNights = document.getElementById('btn_nights');
 
 	sectionTechnical.style.display = "block";
-	btnTechnical.style.fontSize = "20px";
+	btnTechnical.style.fontSize = "1.5em";
+	btnTechnical.style.color="white";
 	btnTechnical.style.borderBottom = "2px solid #FFF"
 
 	sectionManagerial.style.display = "none";
-	btnManagerial.style.fontSize = "14px";
+	btnManagerial.style.fontSize = "1em";
 	btnManagerial.style.borderBottom = "none"
 
 	sectionInformals.style.display = "none";
-	btnInformals.style.fontSize = "14px";
+	btnInformals.style.fontSize = "1em";
 	btnInformals.style.borderBottom = "none"
 
 	sectionNights.style.display = "none";
-	btnNights.style.fontSize = "14px";
+	btnNights.style.fontSize = "1em";
 	btnNights.style.borderBottom = "none"
 
 }
 
 function showTechnical(){
-
+	
 	sectionTechnical = document.getElementById('section_technical');
 	sectionManagerial = document.getElementById('section_managerial');
 	sectionInformals = document.getElementById('section_informals');
@@ -39,19 +41,23 @@ function showTechnical(){
 	btnNights = document.getElementById('btn_nights');
 
 	sectionTechnical.style.display = "block";
-	btnTechnical.style.fontSize = "20px";
+	btnTechnical.style.fontSize = "1.5em";
+	btnTechnical.style.color="white";
 	btnTechnical.style.borderBottom = "2px solid #FFF"
 
 	sectionManagerial.style.display = "none";
-	btnManagerial.style.fontSize = "14px";
+	btnManagerial.style.fontSize = "1em";
+	btnManagerial.style.color="#e0e0d1";
 	btnManagerial.style.borderBottom = "none"
 
 	sectionInformals.style.display = "none";
-	btnInformals.style.fontSize = "14px";
+	btnInformals.style.fontSize = "1em";
+	btnInformals.style.color="#e0e0d1";
 	btnInformals.style.borderBottom = "none"
 
 	sectionNights.style.display = "none";
-	btnNights.style.fontSize = "14px";
+	btnNights.style.fontSize = "1em";
+	btnNights.style.color="#e0e0d1";
 	btnNights.style.borderBottom = "none"
 
 }
@@ -68,19 +74,23 @@ function showManagerial(){
 	btnNights = document.getElementById('btn_nights');
 
 	sectionTechnical.style.display = "none";
-	btnTechnical.style.fontSize = "14px";
+	btnTechnical.style.fontSize = "1em";
+	btnTechnical.style.color="#e0e0d1";
 	btnTechnical.style.borderBottom = "none"
 
 	sectionManagerial.style.display = "block";
-	btnManagerial.style.fontSize = "20px";
+	btnManagerial.style.fontSize = "1.5em";
+	btnManagerial.style.color="white";
 	btnManagerial.style.borderBottom = "2px solid #FFF"
 
 	sectionInformals.style.display = "none";
-	btnInformals.style.fontSize = "14px";
+	btnInformals.style.fontSize = "1em";
+	btnInformals.style.color="#e0e0d1";
 	btnInformals.style.borderBottom = "none"
 
 	sectionNights.style.display = "none";
-	btnNights.style.fontSize = "14px";
+	btnNights.style.fontSize = "1em";
+	btnNights.style.color="#e0e0d1";
 	btnNights.style.borderBottom = "none"
 
 }
@@ -96,20 +106,24 @@ function showInformals(){
 	btnInformals = document.getElementById('btn_informals');
 	btnNights = document.getElementById('btn_nights');
 
+	btnTechnical.style.fontSize = "1em";
 	sectionTechnical.style.display = "none";
-	btnTechnical.style.fontSize = "14px";
+	btnTechnical.style.color="#e0e0d1";
 	btnTechnical.style.borderBottom = "none"
 
 	sectionManagerial.style.display = "none";
-	btnManagerial.style.fontSize = "14px";
+	btnManagerial.style.fontSize = "1em";
+	btnManagerial.style.color="#e0e0d1";
 	btnManagerial.style.borderBottom = "none"
 
 	sectionInformals.style.display = "block";
-	btnInformals.style.fontSize = "20px";
+	btnInformals.style.fontSize = "1.5em";
+	btnInformals.style.color="white";
 	btnInformals.style.borderBottom = "2px solid #FFF"
 
 	sectionNights.style.display = "none";
-	btnNights.style.fontSize = "14px";
+	btnNights.style.fontSize = "1em";
+	btnNights.style.color="#e0e0d1";
 	btnNights.style.borderBottom = "none"
 
 }
@@ -126,19 +140,23 @@ function showNights(){
 	btnNights = document.getElementById('btn_nights');
 
 	sectionTechnical.style.display = "none";
-	btnTechnical.style.fontSize = "14px";
+	btnTechnical.style.fontSize = "1em";
+	btnTechnical.style.color="#e0e0d1";
 	btnTechnical.style.borderBottom = "none"
 
 	sectionManagerial.style.display = "none";
-	btnManagerial.style.fontSize = "14px";
+	btnManagerial.style.fontSize = "1em";
+	btnManagerial.style.color="#e0e0d1";
 	btnManagerial.style.borderBottom = "none"
 
 	sectionInformals.style.display = "none";
-	btnInformals.style.fontSize = "14px";
+	btnInformals.style.fontSize = "1em";
+	btnInformals.style.color="#e0e0d1";
 	btnInformals.style.borderBottom = "none"
 
 	sectionNights.style.display = "block";
-	btnNights.style.fontSize = "20px";
+	btnNights.style.fontSize = "1.5em";
+	btnNights.style.color="white";
 	btnNights.style.borderBottom = "2px solid #FFF"
 
 }
